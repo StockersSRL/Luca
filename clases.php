196e33dc70324e6e0fe792be3a59a3dba485606a
Lucas Rodriguez <lucas.rodriguez@outlook.com>
2016-02-05 15:05:20 -0300

    Agregue la clase para la base de datos

--- clases.php
+++ clases.php
@@ -7,7 +7,7 @@ class bd{
 	
 	public static function connect(){
 		define('DB_SERVER','localhost'); 
-		define('DB_NAME','b3'); 
+		define('DB_NAME','sm'); 
 		define('DB_USER','root'); 
 		define('DB_PASS',''); 
 		bd::$con = @mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
