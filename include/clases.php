abfe0a38f31296ce4c917be8d14863571fe29570
Lucas Rodriguez <lucas.rodriguez@outlook.com>
2016-02-07 03:58:36 -0300

    No logre nada con esto

--- include/clases.php
+++ include/clases.php
@@ -10,7 +10,6 @@ define('DB_PASS','');
 BD::connect();
 
 class BD{
-	
 	public static $con;
 	public static function connect(){
 		BD::$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
@@ -26,26 +25,67 @@ class IMG{
     public $src;
     public $width;
     public $height;
-    public $imageType;
-    public $mime;
+    public $imageType;//PHP constant
+    public $mime;// e.g. "image/png"
     //Position top left
-    public $x;
-    public $y;
+    public $x=0;
+    public $y=0;
     public $maxWidth;
     public $maxHeight;
+    public $canvasWidth;
+    public $canvasHeight;
 
     function __construct($src){
+        $this->src=$src;
         $array=  getimagesize($src);
-        $this->width=$array[0];
-        $this->height=$array[1];
+        // http://php.net/manual/en/function.getimagesize.php
+        $this->width=$array[0]/72*25.4;
+        $this->height=$array[1]/72*25.4;
         $this->imagetype=$array[2];
         $this->mime=$array['mime'];
+    }
+    
+    function alignHorizontal(){
+        $this->x=($this->canvasWidth - $this->width)/2;
+    }
+    
+    function alignVertical(){
+        $this->y=($this->canvasHeight - $this->height)/2;
+    }
+    
+    function center(){
+        $this->alignHorizontal();
+        $this->alignVertical();
+    }
+    
+    function resize($k){
+        $centerX = $this->x+$this->width/2;
+        $centerY = $this->y+$this->height/2;
+        $this->width *= $k;
+        $this->height *= $k;
+        $this->x = $centerX - $this->width;
+        $this->y = $centerY - $this->height;
     }
 }
 
 class PDF extends FPDF{
-    function Image(IMG $img){
+    function img(IMG $img){
+        $originalProportion=$img->width/$img->height;
+        $maxProportion=$img->maxWidth/$img->maxHeight;
+        var_dump($img);
+        if($originalProportion > $maxProportion){
+            $img->resize($maxProportion);
+        }else{
+            $img->resize($originalProportion);
+        }
+        var_dump($img);
         parent::Image($img->src, $img->x, $img->y, $img->width, $img->height);
+    }
+    
+    function setImgCanvasSize(IMG $img){
+        $img->canvasWidth=$this->w;
+        $img->canvasHeight=$this->h;
+        return $img;
     }
 }
 
--- index.php
+++ index.php
@@ -1,94 +1,19 @@
 <?php
-require "include/fpdf.php";
 require "include/clases.php";
-class PDF extends FPDF{/*
-	function Header(){
-	global $lci;
-	global $lnombre;
-	global $luser;
-	global $lsucursal;
-	global $lcargo;
-	global $pdf;
-	global $i;
 
-		$this->SetFont("Arial", "b", 19);
-		$this->SetTextColor(0);
-		$this->Cell(0, 5, "Listado de usuarios", 0, 1, 'C');
-		$this->Ln();
-		
-		$this->SetFillColor(255);
-		$this->SetTextColor(0);
-		$this->SetFont("Arial", "b", 12);
-		$this->Cell($lci, 5, "CI", 1, 0, 'C');
-		$this->Cell($lnombre, 5, "Nombre", 1, 0, 'C');
-		$this->Cell($luser, 5, "Username", 1, 0, 'C');
-		$this->Cell($lsucursal, 5, "Sucursal", 1, 0, 'C');
-		$this->Cell($lcargo, 5, "Cargo", 1, 0, 'C');
-		$this->Ln();
-	}
-	
-	function Footer(){
-	global $hoy;
-	// $phpdate = strtotime($hoy);
-	$meses=array(1=>"enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre");
-	$ano = date_format($hoy, "Y");
-	$mes =(int) date_format($hoy, "m");
-	$dia = date_format($hoy, "j");
-	
-	$this->Ln();
-	$this->SetTextColor(0);
-	$this->Cell((297-10-10)/2, 5, "$dia de $meses[$mes] de $ano, ".date_format($hoy, "H:i:s"), 0, 0, "L");
-	$this->Cell((297-10-10s)/2, 5, utf8_decode("Página ").$this->PageNo()."/{nb}", 0, 0, "R");
-	}*/
-
-    const DPI = 96;
-    const MM_IN_INCH = 25.4;
-    const A4_HEIGHT = 210;
-    const A4_WIDTH = 297;
-    // tweak these values (in pixels)
-    const MAX_WIDTH = 800;
-    const MAX_HEIGHT = 500;
-    function pixelsToMM($val) {
-        return $val * self::MM_IN_INCH / self::DPI;
-    }
-    function resizeToFit($imgFilename) {
-        list($width, $height) = getimagesize($imgFilename);
-        $widthScale = self::MAX_WIDTH / $width;
-        $heightScale = self::MAX_HEIGHT / $height;
-        $scale = min($widthScale, $heightScale);
-        return array(
-            round($this->pixelsToMM($scale * $width)),
-            round($this->pixelsToMM($scale * $height))
-        );
-    }
-    function centreImage($img) {
-        list($width, $height) = $this->resizeToFit($img);
-        // you will probably want to swap the width/height
-        // around depending on the page's orientation
-        $this->Image(
-            $img, (self::A4_HEIGHT - $width) / 2,
-            (self::A4_WIDTH - $height) / 2,
-            $width,
-            $height
-        );
-    }
-    
-    
-    
-    
-}
 $pdf=new PDF('P', 'mm', 'A4');
-$pdf->SetMargins(10, 10, 10);
 $pdf->AliasNbPages();
-
+/*
 $hoy = date_create();
 $lci=21;
 $lnombre=80;
 $luser=40;
 $lsucursal=20;
 $lcargo=80+30+5;
+*/
 
 $pdf->AddPage();
+
 /*
 $sql="SELECT persona.CI, Nombre, Apellido, User, Sucursal, Cargo FROM empleado, persona WHERE empleado.CI = persona.CI";
 $resultado=mysqli_query(bd::$con, $sql);
@@ -119,7 +44,12 @@ for($i=0; $i<50; $i++){
 	$pdf->Ln();
 }*/
 
+$img = new IMG('fontanero.jpg');
+$pdf->setImgCanvasSize($img);
+$img->maxHeight=98;
+$img->maxWidth=96;
+$img->center();
+$pdf->img($img);
 
-$pdf->centreImage('fontanero.jpg');
 $pdf->Output('test.pdf', 'I');
 ?>
